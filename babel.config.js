be615634d70723ca7cadf3ddb0a8b08e72913245
Luke Schoen <ltfschoen@gmail.com>
2019-01-30 17:17:23 +1100

    chore: WIP errors

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,12 @@
 module.exports = {
   plugins: [
-    '@babel/plugin-proposal-class-properties'
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-object-rest-spread',
+    '@babel/transform-runtime'
   ],
   presets: [
     '@babel/preset-env',
     '@babel/preset-react',
-    'react-native'
+    'module:metro-react-native-babel-preset'
   ]
 };
